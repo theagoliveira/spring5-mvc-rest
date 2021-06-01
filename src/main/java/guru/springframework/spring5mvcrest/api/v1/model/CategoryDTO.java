ab50a17cf367323f8a2d1f36487839cb5d2e2c2c
Thiago Cavalcante <thiago.kun@gmail.com>
2021-06-01 01:22:11 -0300

    Add Category related classes

---
+++ src/main/java/guru/springframework/spring5mvcrest/api/v1/model/CategoryDTO.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring5mvcrest.api.v1.model;
+
+public class CategoryDTO {
+
+    private Long id;
+    private String name;
+
+}
---
+++ src/main/java/guru/springframework/spring5mvcrest/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring5mvcrest.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import guru.springframework.spring5mvcrest.domain.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
