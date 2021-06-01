3e081070acc3d352c1856522b82f4c0185766933
Thiago Cavalcante <thiago.kun@gmail.com>
2021-06-01 01:29:23 -0300

    Add @Entity annotation to Category

--- src/main/java/guru/springframework/spring5mvcrest/domain/Category.java
+++ src/main/java/guru/springframework/spring5mvcrest/domain/Category.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5mvcrest.domain;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -7,6 +8,7 @@ import javax.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Category {
 
     @Id
